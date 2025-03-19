7b2e7b72c2cd6b7e3137623173e5bd49a17d7304
Sohan Puthran <puthran.sohan@gmail.com>
2025-03-19 17:24:32 -0500

    Day 6-7 complete

--- Day6-7_Python-Functions-and-Hangman/hangman.py
+++ Day6-7_Python-Functions-and-Hangman/hangman.py
@@ -1,19 +1,51 @@
 import random
+from hangman_arts import hangman_logo, hangman_stages
 
-word_list = ['ardvark', 'baboon', 'camel']
 
-# TODO 1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+with open("Day6-7_Python-Functions-and-Hangman\hangman_words.txt", "r") as f:
+    word_list = f.read().split()
+
+print(hangman_logo)
+print("Welcome to Hangman!")
+print("****************************************")
+
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO 2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
-print(guess)
-
-# TODO 3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print 'Right' if it is correct, 
-# print 'Wrong' if it is wrong. 
+display = ""
 for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+    display += "_"
+print(display)
+
+lives = 6
+game_over = False
+
+while not game_over:
+    print(f"****************************************{lives}/6 LIVES LEFT****************************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            display = display[:position] + guess + display[position + 1:]
+    
+    print("Word to guess:", display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"{guess} is not in the word. You lose a life.")
+        print(f"Number of life left: {lives}")
+        if lives == 0:
+            game_over = True
+            print(hangman_stages[6 - lives])
+            print(f"****************************************THE WORD WAS {chosen_word}. You lose!****************************************")
+            break
+
+    if "_" not in display:
+        print("****************************************You win!****************************************")
+        break
+    
+    print(hangman_stages[6 - lives])
+    
